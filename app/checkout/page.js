c6734bdd5fef4e483a02765e5cbff48b5bc60ae9
liba-ny <94902436+liba-ny@users.noreply.github.com>
2023-02-16 23:57:32 +0100

    Fix checkout form

--- app/checkout/page.js
+++ app/checkout/page.js
@@ -44,29 +44,25 @@ export default async function Checkout() {
   }, 0);
 
   return (
-    <div className={styles.body}>
-      {/* <h1 className = {styles.total}>Total: $ {totalCost / 100}</h1>
+    <div className={styles.container}>
+      <h1 className={styles.h1}>Total: $ {totalCost / 100}</h1>
       <form className={styles.form}>
-        <div className={styles.name}>
-          <div>
-            <label htmlFor="firstName">First Name:</label>
-            <input
-              className={styles.input}
-              data-test-id="checkout-first-name"
-              id="firstName"
-              required
-            />
-          </div>
-          <div>
-            <label htmlFor="lastName">Last Name:</label>
-            <input
-              className={styles.input}
-              data-test-id="checkout-last-name"
-              id="lastName"
-              required
-            />
-          </div>
-        </div>
+        <label htmlFor="firstName">First Name:</label>
+        <input
+          className={styles.input}
+          data-test-id="checkout-first-name"
+          id="firstName"
+          required
+        />
+
+        <label htmlFor="lastName">Last Name:</label>
+        <input
+          className={styles.input}
+          data-test-id="checkout-last-name"
+          id="lastName"
+          required
+        />
+
         <label htmlFor="email">Email:</label>
         <input
           className={styles.input}
@@ -94,7 +90,7 @@ export default async function Checkout() {
 
         <label htmlFor="postalCode">Postal Code:</label>
         <input
-          className={styles.input}
+          className={styles.smlinput}
           data-test-id="checkout-postal-code"
           id="postalCode"
           type="number"
@@ -136,105 +132,23 @@ export default async function Checkout() {
 
         <label htmlFor="securityCode">Security Code:</label>
         <input
-          className={styles.input}
+          className={styles.smlinput}
           data-test-id="checkout-security-code"
           type="password"
           id="securityCode"
           required
         />
       </form>
-      <Link href="/thanks">
-        <button
-          className={styles.confirm}
-          data-test-id="checkout-confirm-order"
-        >
-          Confirm Order
-        </button>
-      </Link>
+      <div className={styles.buttoncontainer}>
+        <Link className={styles.link} href="/thanks">
+          <button
+            className={styles.confirm}
+            data-test-id="checkout-confirm-order"
+          >
+            Confirm Order
+          </button>
+        </Link>
+      </div>
     </div>
   );
-} */}
-
-<div class="row">
-  <div class="col-75">
-    <div class="container">
-      <form action="/action_page.php">
-
-        <div class="row">
-          <div class="col-50">
-            <h3>Billing Address</h3>
-            <label for="fname"><i class="fa fa-user"></i> Full Name</label>
-            <input type="text" id="fname" name="firstname" placeholder="John M. Doe">
-            <label for="email"><i class="fa fa-envelope"></i> Email</label>
-            <input type="text" id="email" name="email" placeholder="john@example.com">
-            <label for="adr"><i class="fa fa-address-card-o"></i> Address</label>
-            <input type="text" id="adr" name="address" placeholder="542 W. 15th Street">
-            <label for="city"><i class="fa fa-institution"></i> City</label>
-            <input type="text" id="city" name="city" placeholder="New York">
-
-            <div class="row">
-              <div class="col-50">
-                <label for="state">State</label>
-                <input type="text" id="state" name="state" placeholder="NY">
-              </div>
-              <div class="col-50">
-                <label for="zip">Zip</label>
-                <input type="text" id="zip" name="zip" placeholder="10001">
-              </div>
-            </div>
-          </div>
-
-          <div class="col-50">
-            <h3>Payment</h3>
-            <label for="fname">Accepted Cards</label>
-            <div class="icon-container">
-              <i class="fa fa-cc-visa" style="color:navy;"></i>
-              <i class="fa fa-cc-amex" style="color:blue;"></i>
-              <i class="fa fa-cc-mastercard" style="color:red;"></i>
-              <i class="fa fa-cc-discover" style="color:orange;"></i>
-            </div>
-            <label for="cname">Name on Card</label>
-            <input type="text" id="cname" name="cardname" placeholder="John More Doe">
-            <label for="ccnum">Credit card number</label>
-            <input type="text" id="ccnum" name="cardnumber" placeholder="1111-2222-3333-4444">
-            <label for="expmonth">Exp Month</label>
-            <input type="text" id="expmonth" name="expmonth" placeholder="September">
-
-            <div class="row">
-              <div class="col-50">
-                <label for="expyear">Exp Year</label>
-                <input type="text" id="expyear" name="expyear" placeholder="2018">
-              </div>
-              <div class="col-50">
-                <label for="cvv">CVV</label>
-                <input type="text" id="cvv" name="cvv" placeholder="352">
-              </div>
-            </div>
-          </div>
-
-        </div>
-        <label>
-          <input type="checkbox" checked="checked" name="sameadr"> Shipping address same as billing
-        </label>
-        <input type="submit" value="Continue to checkout" class="btn">
-      </form>
-    </div>
-  </div>
-
-  <div class="col-25">
-    <div class="container">
-      <h4>Cart
-        <span class="price" style="color:black">
-          <i class="fa fa-shopping-cart"></i>
-          <b>4</b>
-        </span>
-      </h4>
-      <p><a href="#">Product 1</a> <span class="price">$15</span></p>
-      <p><a href="#">Product 2</a> <span class="price">$5</span></p>
-      <p><a href="#">Product 3</a> <span class="price">$8</span></p>
-      <p><a href="#">Product 4</a> <span class="price">$2</span></p>
-      <hr>
-      <p>Total <span class="price" style="color:black"><b>$30</b></span></p>
-    </div>
-  </div>
-</div>
+}
--- app/pastries/page.js
+++ app/pastries/page.js
@@ -1,6 +1,7 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import { getPastries } from '../../database/pastries';
+import styles from './page.module.scss';
 
 // import styles from './page.module.scss';
 
@@ -19,28 +20,30 @@ export default async function PastriesPage() {
   const pastries = await getPastries();
   console.log(pastries);
   return (
-    <div>
-      <h1>Pastries</h1>
+    <>
+      <h1 className={styles.h1}>Pastries</h1>
       <main>
-        {pastries.map((pastry) => {
-          return (
-            <div key={pastry.id}>
-              <Link href={`/pastries/${pastry.id}`}>
-                <h2>{pastry.name}</h2>
-              </Link>
-              <Link href={`/pastries/${pastry.id}`}>
-                <Image
-                  src={`/${pastry.name}.jpg`}
-                  alt={`${pastry.name} image`}
-                  height="200"
-                  width="200"
-                />
-              </Link>
-              <h3>{pastry.price}</h3>
-            </div>
-          );
-        })}
+        <div className={styles.list}>
+          {pastries.map((pastry) => {
+            return (
+              <div key={pastry.id}>
+                <Link href={`/pastries/${pastry.id}`}>
+                  <h2>{pastry.name}</h2>
+                </Link>
+                <Link href={`/pastries/${pastry.id}`}>
+                  <Image
+                    src={`/${pastry.name}.jpg`}
+                    alt={`${pastry.name} image`}
+                    height="200"
+                    width="200"
+                  />
+                </Link>
+                <h3>${pastry.price / 100}</h3>
+              </div>
+            );
+          })}
+        </div>
       </main>
-    </div>
+    </>
   );
 }
