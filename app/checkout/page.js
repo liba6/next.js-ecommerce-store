c48d481f45c11b9669b14734825df07325fc01c2
liba-ny <94902436+liba-ny@users.noreply.github.com>
2023-02-22 17:20:28 +0100

    Remove space

--- app/checkout/page.js
+++ app/checkout/page.js
@@ -37,7 +37,6 @@ export default async function Checkout() {
   pastriesWithAmt.map((pastry) => {
     return pastry.amount > 0 ? orderedPastries.push(pastry) : null;
   });
-  // console.log('pastriesonly', orderedPastries);
 
   const totalCost = pastriesWithAmt.reduce((prevCost, currentCost) => {
     return Number(prevCost) + Number(currentCost.amount * currentCost.price);
@@ -47,109 +46,6 @@ export default async function Checkout() {
     <div className={styles.container}>
       <h1 className={styles.h1}>Total: $ {totalCost / 100}</h1>
       <CheckoutComponent />
-      {/* <form className={styles.form}>
-        <label htmlFor="firstName">First Name:</label>
-        <input
-          className={styles.input}
-          data-test-id="checkout-first-name"
-          id="firstName"
-          required
-        />
-
-        <label htmlFor="lastName">Last Name:</label>
-        <input
-          className={styles.input}
-          data-test-id="checkout-last-name"
-          id="lastName"
-          required
-        />
-
-        <label htmlFor="email">Email:</label>
-        <input
-          className={styles.input}
-          data-test-id="checkout-email"
-          id="email"
-          type="email"
-          required
-        />
-
-        <label htmlFor="address">Address:</label>
-        <input
-          className={styles.input}
-          data-test-id="checkout-address"
-          id="address"
-          required
-        />
-
-        <label htmlFor="city">City:</label>
-        <input
-          className={styles.input}
-          data-test-id="checkout-city"
-          id="city"
-          required
-        />
-
-        <label htmlFor="postalCode">Postal Code:</label>
-        <input
-          className={styles.smlinput}
-          data-test-id="checkout-postal-code"
-          id="postalCode"
-          type="number"
-          required
-        />
-
-        <label htmlFor="country">Country:</label>
-        <input
-          className={styles.input}
-          data-test-id="checkout-country"
-          id="country"
-          required
-        />
-
-        <label htmlFor="cc">Credit Cart Number:</label>
-        <input
-          className={styles.input}
-          data-test-id="checkout-credit-card"
-          id="cc"
-          type="tel"
-          inputMode="numeric"
-          pattern="[0-9\s]{13,19}"
-          autoComplete="cc-number"
-          maxLength="19"
-          placeholder="xxxx xxxx xxxx xxxx"
-          required
-        />
-
-        <label htmlFor="expirationDate">Expires:</label>
-        <input
-          className={styles.input}
-          data-test-id="checkout-expiration-date"
-          type="number"
-          placeholder="MM / YY"
-          maxLength="5"
-          id="expirationDate"
-          required
-        />
-
-        <label htmlFor="securityCode">Security Code:</label>
-        <input
-          className={styles.smlinput}
-          data-test-id="checkout-security-code"
-          type="password"
-          id="securityCode"
-          required
-        />
-      </form>
-      <div className={styles.buttoncontainer}>
-        <Link className={styles.link} href="/thanks">
-          <button
-            className={styles.confirm}
-            data-test-id="checkout-confirm-order"
-          >
-            Confirm Order
-          </button>
-        </Link>
-      </div> */}
     </div>
   );
 }
